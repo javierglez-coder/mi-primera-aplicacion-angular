a4cc0262c29d1a7bbb242c6a8e75265f1e9c592e
Jimmy Javier <jimmyjavier@gmail.com>
2025-09-22 10:39:10 -0600

    Ejercicios Event Binding

--- src/app/eventos/eventos.ts
+++ src/app/eventos/eventos.ts
@@ -9,6 +9,21 @@ import { Component } from '@angular/core';
     <button (click)="incrementar()">Incrementar</button>
     <button (click)="decrementar()">Decrementar</button>
     <div class="caja" (mouseover)="onMouseOver()"></div>
+    <hr />
+    <div>
+      <input type="text" (input)="alEscribir($event)" [value]="mensaje" placeholder="Escribe un mensaje" />
+      <p>{{mensaje}}</p>
+   </div>
+   <div>
+      <h3>Lista de Tareas</h3>
+      <input type="text" (input)="alEscribirItem($event)" placeholder="Nueva tarea" [value]="nuevaTarea" />
+      <button (click)="agregarTarea()">Agregar Nueva Tarea</button>
+      <ul>
+        @for(item of items;track item){
+          <li>{{item}}</li>
+        }
+      </ul>
+   </div>
   `,
   styles: [
     `
@@ -22,7 +37,9 @@ import { Component } from '@angular/core';
 })
 export class Eventos {
   contador:number=0;
-
+  mensaje:string="";
+  items:string[]=["Comprar Guitarra ESP","Pasear al perro","Lavar el coche"];
+  nuevaTarea:string="";
   incrementar(){
     console.log("Incrementar");
     this.contador++;
@@ -35,6 +52,24 @@ export class Eventos {
 
   onMouseOver(){
     console.log("Mouse Over");
+  }
+
+  alEscribir(event:Event){
+    const input = event.target as HTMLInputElement;
+    this.mensaje = input.value;
+  }
+
+  alEscribirItem(event:Event){
+    const input = event.target as HTMLInputElement;
+    this.nuevaTarea = input.value;  
+  }
+
+  agregarTarea(){
+    if(this.nuevaTarea.trim().length===0){
+      return;
+    }
+    this.items.push(this.nuevaTarea);
+    this.nuevaTarea="";
   }
 
 }
